109ec6300baceb7e1bc29e49ded4a75d4fef418b
Nam Kaito7love <104958975+kaito7love@users.noreply.github.com>
2024-11-05 00:48:01 +0700

    nam

---
+++ src/server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import bodyParser from "body-parser";
+import viewEngine from "./config/viewEngine";
+import initWebRoutes from "./route/web";
+require("dotenv").config();
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json);
+app.use(bodyParser.urlencoded({ extended: true }));
+
+viewEngine(app);
+initWebRoutes(app);
+
+// let port = process.env.port || 6969;
+// port undefine port = 6969
+
+app.listen(port, () => {
+    //call back
+    console.log(`Server is running on http://localhost:${port}`);
+});
