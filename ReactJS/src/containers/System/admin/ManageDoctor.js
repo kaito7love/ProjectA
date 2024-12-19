100531b4c3ae01334a0c232a825fbf623c782e00
Nam Kaito7love <104958975+kaito7love@users.noreply.github.com>
2024-12-19 19:12:57 +0700

    update

--- ReactJS/src/containers/System/admin/ManageDoctor.js
+++ ReactJS/src/containers/System/admin/ManageDoctor.js
@@ -5,16 +5,21 @@ import { connect } from "react-redux";
 import "./ManageDoctor.scss";
 import { Remarkable } from "remarkable"; // Import Remarkable
 import * as actions from "../../../store/actions";
-
+import Select from 'react-select';
 const md = new Remarkable(); // Khởi tạo Remarkable
-
+const options = [
+    { value: 'chocolate', label: 'Chocolate' },
+    { value: 'strawberry', label: 'Strawberry' },
+    { value: 'vanilla', label: 'Vanilla' },
+];
 class ManageDoctor extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            arrUsers: [],
             postContent: "", // Nội dung Markdown
-            renderedHTML: "" // HTML đã được render từ Markdown
+            renderedHTML: "", // HTML đã được render từ Markdown
+            selectedDoctor: '',
+            description: '',
         };
     }
 
@@ -24,10 +29,15 @@ class ManageDoctor extends Component {
 
     componentDidUpdate(prevProps, prevState) {
         if (prevProps.users !== this.props.users) {
-            this.setState({ arrUsers: this.props.users });
+            // this.setState({ arrUsers: this.props.users });
         }
     }
-
+    handleDescriptionChange = (event) => {
+        this.setState({
+            description: event.target.value,
+           
+        });
+    }
     handleEditorChange = (e) => {
         const markdownText = e.target.value;
         const renderedHTML = md.render(markdownText); // Render Markdown sang HTML
@@ -36,10 +46,16 @@ class ManageDoctor extends Component {
             postContent: markdownText,
             renderedHTML: renderedHTML
         });
-    };
 
-    handleSaveContentMarkdown=()=>{
-     
+    };
+    handleChange = (selectedDoctor) => {
+        this.setState({ selectedDoctor }, () =>
+            console.log(`Option selected:`, this.state.selectedDoctor)
+        );
+    };
+    handleSaveContentMarkdown = () => {
+        console.log("check state", this.state);
+
     }
     render() {
         return (
@@ -47,9 +63,24 @@ class ManageDoctor extends Component {
                 <div className="title">Doctor Manage</div>
                 <div className="markdown">
                     <div className="markdown-title">Thêm thông tin bác sĩ:</div>
-                    <div>
-                        
+                    <div className="markdown-doctor-detail">
+                        <div className="markdown-info-left">
+                            <div>Chon Bac Si</div>
+                            <Select
+                                className="input-info-left"
+                                value={this.state.selectedDoctor}
+                                onChange={this.handleChange}
+                                options={options}
+                            />
+                        </div>
+                        <div className="markdown-info-right">
+                            <div>Thong tin gioi thieu</div>
+                            <textarea rows={4} className="input-info-right"
+                                value={this.state.description}
+                                onChange={(event) => { this.handleDescriptionChange(event) }} />
+                        </div>
                     </div>
+                    <div>Them Bai viet</div>
                     <div className="markdown-content">
                         <textarea
                             className="input-markdown"
